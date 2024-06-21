5af60a3d55644950a298623677b8d9d0c3f5c945
Akhin <akhintheruvath2827@gmail.com>
2024-06-21 13:02:50 +0530

    tailwind initialization and setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '0',
+      },
+      margin: {
+        DEFAULT: '0',
+      },
+    },
+  },
+  plugins: [],
+}
